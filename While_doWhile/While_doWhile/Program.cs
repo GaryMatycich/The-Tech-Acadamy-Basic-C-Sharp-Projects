b5949da5bea05e4b7d33ee7f9afe8da14671b78f
GaryMatycich <garymatycich@gmail.com>
2021-04-30 19:13:21 -0600

    add while_dowhile assignment

---
+++ While_doWhile/While_doWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_doWhile
+{
+    class Program
+    {
+        static void Main()
+        {
+            // starts a while loop that increments variable i by 1 while i is less then 10
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
+
+            //--------------------------
+
+            // starts a do while loop that adds 5 to x while x is less then 50
+            // this block executes before the condition is checked
+            int x = 5;
+            do
+            {
+                Console.WriteLine(x);
+                x += 5;
+            }
+            while (x < 50);
+            Console.ReadLine();
+        }
+    }
+}
