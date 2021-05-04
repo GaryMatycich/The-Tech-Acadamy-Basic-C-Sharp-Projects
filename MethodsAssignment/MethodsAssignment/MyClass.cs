b71e688e7a0ce34715ae2dac38e1b80455e23660
GaryMatycich <garymatycich@gmail.com>
2021-05-04 16:40:57 -0600

    add methods assignment

---
+++ MethodsAssignment/MethodsAssignment/MyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class MyClass
+    {
+        // creating a new public method that takes in an int and returns an int
+        public int MyAdd(int num)
+        {
+            int result = num + 5;
+            return result;
+        }
+
+        public int MySubtract(int num)
+        {
+            int result = num - 2;
+            return result;
+        }
+
+        public int MyMultiply(int num)
+        {
+            int result = num * 10;
+            return result;
+        }
+    }
+}
---
+++ MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating a new object of type MyClass
+            MyClass myClass = new MyClass();
+
+            Console.WriteLine("Please enter a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // calling the methods of MyClass to act on the user input
+            int output1 = myClass.MyAdd(userInput);
+            Console.WriteLine(output1);
+            int output2 = myClass.MySubtract(userInput);
+            Console.WriteLine(output2);
+            int output3 = myClass.MyMultiply(userInput);
+            Console.WriteLine(output3);
+
+            Console.ReadLine();
+        }
+    }
+}
