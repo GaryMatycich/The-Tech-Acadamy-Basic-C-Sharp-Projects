c4db59ed83043fff515480f38e47070e58778b04
GaryMatycich <garymatycich@gmail.com>
2021-05-05 15:10:48 -0600

    method class assignment

---
+++ Method Class/Method Class/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    public class MyClass
+    {
+        public void MyMethod(int num1, int num2)
+        {
+            int newNum = num1 + 10;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ Method Class/Method Class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            // calling MyMethod and passing in two parameters
+            myClass.MyMethod(4, 6);
+            
+            // calling MyMethod and specifying parameters by name
+            myClass.MyMethod(num1: 4, num2: 8);
+        }
+    }
+}
