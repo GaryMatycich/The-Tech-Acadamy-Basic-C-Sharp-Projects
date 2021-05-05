6e6949f2b5653073767fc078755ebb2dca29613c
GaryMatycich <garymatycich@gmail.com>
2021-05-04 18:53:35 -0600

    add main method assignment

---
+++ MainMethod/MainMethod/MyClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class MyClass
+    {
+        public int MyMethod(int num)
+        {
+            int output = num + 10;
+            return output;
+        }
+
+        public int MyMethod(decimal num)
+        {
+            // converting a decimal to a int
+            decimal outputDecimal = 20 * num;
+            int output = Convert.ToInt32(outputDecimal);
+            return output;
+        }
+
+        public int MyMethod(string num)
+        {
+            // converting a string to a int
+            int numInt = Convert.ToInt32(num);
+            int output = 50 / numInt;
+            return output;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiating a new object of type MyClass
+            MyClass myClass = new MyClass();
+
+            // calling a method of the object myClass
+            int result1 = myClass.MyMethod(5);
+            Console.WriteLine(result1);
+
+            // calling an overload method
+            int result2 = myClass.MyMethod(5.54m);
+            Console.WriteLine(result2);
+
+            int result3 = myClass.MyMethod("10");
+            Console.WriteLine(result3);
+
+            Console.ReadLine();
+        }
+    }
+}
