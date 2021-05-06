39d752dfc253d0d1a2c2546d19d1eaf3d79ebbe4
GaryMatycich <garymatycich@gmail.com>
2021-05-06 15:04:23 -0600

    add Polymorphism assignment

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person, IQuittable
+    {
+        // this method is required by the interface we are inheriting from
+        public void Quit()
+        {
+            throw new NotImplementedException();
+        }
+
+        // this method is required by our abstract class. the override keyword allows us to augment its implimentation
+        public override void SayName()
+        {
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates a employee object of type IQuittable
+            IQuittable employee = new Employee();
+
+            employee.Quit();
+        }
+    }
+}
