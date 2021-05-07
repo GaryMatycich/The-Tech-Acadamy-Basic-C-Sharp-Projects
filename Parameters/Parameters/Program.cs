764c92d6fbf8ba05ccb0bc730aa4e8913091c993
GaryMatycich <garymatycich@gmail.com>
2021-05-06 20:59:48 -0600

    add parsing enums assignment

---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>()
+            {
+                Things = new List<string>() { "one", "two", "three"}
+            };
+            
+            Employee<int> employee2 = new Employee<int>()
+            {
+                Things = new List<int>() { 1, 234, 45251, 166}
+            };
+
+            foreach (string thing in employee1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+
+            foreach (int thing in employee2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Parsing enums/Parsing enums/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_enums
+{
+    class Program
+    {
+        // creating an enum
+        enum Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saterday, Sunday}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week:");
+            string userInput = Console.ReadLine();
+            try
+            {
+                // parsing user input to find a match in our enum
+                Days dayValue = (Days)Enum.Parse(typeof(Days), userInput, true);
+                // printing the corrisponding enum value to screen
+                Console.WriteLine(dayValue);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
