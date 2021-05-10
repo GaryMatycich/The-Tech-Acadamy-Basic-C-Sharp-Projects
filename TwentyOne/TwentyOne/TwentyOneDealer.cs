98c440ace8a6de89ba7cadbb9057662df1faeb91
GaryMatycich <garymatycich@gmail.com>
2021-05-10 16:26:48 -0600

    add playable version of 21

--- TwentyOne/TwentyOne/TwentyOneDealer.cs
+++ TwentyOne/TwentyOne/TwentyOneDealer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,8 @@ namespace TwentyOne
 {
     public class TwentyOneDealer : Dealer
     {
-        public List<Card> Hand { get; set; }
+        private List<Card> _hand = new List<Card>();
+        public List<Card> Hand { get { return _hand; } set { _hand = value; } }
         public bool Stay { get; set; }
         public bool isBusted { get; set; }
     }
