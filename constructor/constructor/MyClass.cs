2b5832c1dc43fae276e84f10e1e3a4be58aba0c4
GaryMatycich <garymatycich@gmail.com>
2021-05-11 15:04:22 -0600

    add constructor assignment

---
+++ constructor/constructor/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    class MyClass
+    {
+        // This is an alternate constructor used when we dont have arguments to pass or want default settings
+        public MyClass() : this(5, "aWord")
+        {
+        }
+
+        public MyClass(int num1, string word)
+        {
+        }
+    }
+}
---
+++ constructor/constructor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // const keyword used to keep this variable from being altered
+            const int birthYear = 1988;
+
+            // var keyword used to implicitly define a type
+            var ID = new Guid();
+        }
+    }
+}
